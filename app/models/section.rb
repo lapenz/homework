76818631990974abd69b37f0fbd261af6b635900
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2016-05-29 17:11:34 -0300

    Atualizando Section.

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,10 +1,14 @@
 require 'carrierwave/orm/activerecord'
 class Section < ActiveRecord::Base
   belongs_to :lesson
+  has_many :questions
 
   validates :description, presence: true
   validates :lesson_id, presence: true
 
   mount_uploader :audio, AvatarUploader
 
+  def description_detailed
+    self.lesson.description_detailed + ' - ' + description
+  end
 end
