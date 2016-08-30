5d3fe3fe404dd63c918bbb830ff425b1d3e24207
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2016-08-30 09:42:21 -0300

    Atualizando players de audio e tirando capitalize dos fields de resposta.

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -32,7 +32,7 @@ module QuestionsHelper
       for i in 1..count do
         replaceWithFilledFields(description, false)
       end
-      description += "<input value=\"#{userQuestion.question_id}\" type=\"hidden\" name=\"question[id]\">"
+      description += "<input autocapitalize=\"none\" value=\"#{userQuestion.question_id}\" type=\"hidden\" name=\"question[id]\">"
       description
     end
   end
@@ -62,13 +62,13 @@ module QuestionsHelper
     disabled = disabled ? 'disabled' : ''
     originalAnswer = getOriginalAnswer(description)
     answerToFill = originalAnswer.gsub(/[{}%&"]/,'') # remove special characters
-    description.sub!(originalAnswer, "<input class=\"answer-field\" type=\"text\" value=\"#{answerToFill}\" #{disabled} name=\"question[answers][]\" autocomplete=\"off\" style=\"width: #{((originalAnswer.length + 3) * 6.5)}px;\">")
+    description.sub!(originalAnswer, "<input autocapitalize=\"none\" class=\"answer-field\" type=\"text\" value=\"#{answerToFill}\" #{disabled} name=\"question[answers][]\" autocomplete=\"off\" style=\"width: #{((originalAnswer.length + 3) * 6.5)}px;\">")
   end
 
   def replaceWithFields(description)
     originalAnswer = getOriginalAnswer(description)
     answerSize = originalAnswer.length - 2 # para remover os {} da contagem
-    description.sub!(originalAnswer, "<input class=\"answer-field\" type=\"text\" name=\"question[answers][]\" autocomplete=\"off\" maxlength=\"#{answerSize}\" style=\"width: #{((answerSize + 3) * 6.5)}px;\">")
+    description.sub!(originalAnswer, "<input autocapitalize=\"none\" class=\"answer-field\" type=\"text\" name=\"question[answers][]\" autocomplete=\"off\" maxlength=\"#{answerSize}\" style=\"width: #{((answerSize + 3) * 6.5)}px;\">")
   end
 
   def self.replaceWithAnswer(answer, description)
