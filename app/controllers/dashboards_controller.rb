8f8ce257e84551b8eb888bf6361056861a3c848d
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2016-11-10 17:51:29 -0200

    Alterando o redirect do close section.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -32,7 +32,7 @@ class DashboardsController < ApplicationController
         # busca o email do professor
         dados = Professor.joins('inner join turma on turma.professor_id = professor.id').joins('inner join matricula on turma.id = matricula.turma_id').where(:matricula => {aluno_id: current_user.id, status: 2})
         QuestionMailer.send_questions(section, current_user, dados[0].email).deliver_now
-        format.html { redirect_to dashboard_path, notice: 'Questions was successfully delivered.' }
+        format.html { redirect_to result_path(@users_section.section_id), notice: 'Questions was successfully delivered.' }
         format.json { render :show, status: :created, location: @users_section }
       else
         format.html { render :new }
