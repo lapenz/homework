3be78a469161b01172cc2f940a8367e54958df69
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2017-06-30 12:01:08 -0300

    Adicionando metodos para o update_or_create de users_question.

--- app/models/users_question.rb
+++ app/models/users_question.rb
@@ -4,4 +4,14 @@ class UsersQuestion < ActiveRecord::Base
 
   validates_presence_of :user
   validates_presence_of :question
+
+  def self.update_or_create(attributes)
+    assign_or_new(attributes).save
+  end
+
+  def self.assign_or_new(attributes)
+    obj = first || new
+    obj.assign_attributes(attributes)
+    obj
+  end
 end
